e7898be3fe982495a7a8d5b59f2b88b1088423e5
Inbachudar <41140521+Inbachudar@users.noreply.github.com>
2018-07-12 14:24:49 +0530

    Update s2.py

--- s2.py
+++ s2.py
@@ -1,8 +1,8 @@
-a=input()
+a=int(input())
 if(a>=1 and a<=100000):
-	if(int(a)%2== 0):
-		print("Even")
-	else:
-		print("Odd")
+    if((a%2)==0):
+      print("Ëven")
+    else:
+      print("Odd")
 else:
 	print("Invalid")
